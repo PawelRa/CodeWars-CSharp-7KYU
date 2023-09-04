d344d7d8a716cd5f45c20085c46d4da7da34e374
PawelRa <pawelrat@poczta.onet.pl>
2023-09-04 09:27:07 +0200

    Friend or Foe?

---
+++ Friend or Foe/Kata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Friend_or_Foe
+{
+    public static class Kata
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            List<string> list = new List<string>();
+
+            foreach (string name in names)
+            {
+                if (name.Length == 4)
+                {
+                    list.Add(name);
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Friend or Foe/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join(", ", Friend_or_Foe.Kata.FriendOrFoe(new string[] { "Ryan", "Kieran", "Mark", "Jimmy" })));
