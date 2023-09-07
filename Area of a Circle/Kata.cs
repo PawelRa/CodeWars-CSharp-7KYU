168c1d62ab45332e86cf5bfa2987c2054a9e3b45
PawelRa <pawelrat@poczta.onet.pl>
2023-09-07 11:49:56 +0200

    Area of a Circle

---
+++ Area of a Circle/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Area_of_a_Circle
+{
+    public static class Kata
+    {
+        public static double CalculateAreaOfCircle(string radius)
+        {
+            double result = 0;
+
+            var isDouble = double.TryParse(radius, out result);
+
+            if (!isDouble || result <= 0 || radius.Contains(","))
+            {
+                throw new ArgumentException();
+            } 
+
+            Console.WriteLine(isDouble);
+
+            return  Math.Round(Math.PI*result*result,2);
+        }
+    }
+}
---
+++ Area of a Circle/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Area_of_a_Circle.Kata.CalculateAreaOfCircle("-123"));
+
+Console.WriteLine(Area_of_a_Circle.Kata.CalculateAreaOfCircle("43,2673"));
