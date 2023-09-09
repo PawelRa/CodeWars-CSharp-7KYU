2d36b2f36af30af1c9218574b1d5370cb0f2ddea
PawelRa <pawelrat@poczta.onet.pl>
2023-09-09 13:52:14 +0200

    Over The Road - no idea

---
+++ Over The Road/CodeWars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Over_The_Road
+{
+public class CodeWars
+    {
+        public static long OverTheRoad(long address, long n)
+        {
+            long max = n*2;
+
+
+
+
+
+            Console.WriteLine(max - 2);
+
+            // Good luck!
+            return -1;
+        }
+    }
+}
---
+++ Over The Road/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(Over_The_Road.CodeWars.OverTheRoad(3, 5));
+
+Console.WriteLine("--------------");
+
+Console.WriteLine(Over_The_Road.CodeWars.OverTheRoad(7, 11));
