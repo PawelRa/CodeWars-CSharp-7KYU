ea4256a678edd34500850fc8d41600aef23c7904
PawelRa <pawelrat@poczta.onet.pl>
2023-09-11 10:45:39 +0200

    Tail Swap

---
+++ Tail Swap/Kata.cs
@@ -0,0 +1,14 @@
+﻿namespace Tail_Swap
+{
+    public static class Kata
+    {
+        public static string[] TailSwap(string[] arr)
+        {
+            string[] fraze1 = arr[0].Split(':');
+            string[] fraze2 = arr[1].Split(':');
+
+            // your code here
+            return new string[] { $"{fraze1[0]}:{fraze2[1]}", $"{fraze2[0]}:{fraze1[1]}" };
+        }
+    }
+}
---
+++ Tail Swap/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(string.Join(", ", Tail_Swap.Kata.TailSwap(new[] { "abc:123", "cde:456" })));
