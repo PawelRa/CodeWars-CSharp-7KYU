432797a922e86970c57cc3c1f05edbc0fb2e9633
PawelRa <pawelrat@poczta.onet.pl>
2023-09-12 10:30:36 +0200

    Trimming a string

---
+++ Trimming a string/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(Trimming_a_string.TrimStringKata.TrimString("Creating kata is fun", 14));
+
+Console.WriteLine(Trimming_a_string.TrimStringKata.TrimString("Hey", 2));
+
+Console.WriteLine(Trimming_a_string.TrimStringKata.TrimString("Hey", 3));
---
+++ Trimming a string/TrimStringKata.cs
@@ -0,0 +1,13 @@
+﻿namespace Trimming_a_string
+{
+    public class TrimStringKata
+    {
+        public static string TrimString(string phrase, int len)
+        {
+            if (phrase.Length <= len) { return phrase; }
+            if (len <= 3) { return $"{phrase.Substring(0, len)}..."; }
+
+            return $"{phrase.Substring(0, len - 3)}...";
+        }
+    }
+}
