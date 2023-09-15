bf38d2e8cb622146ee4478a3153326c69cd61c21
PawelRa <pawelrat@poczta.onet.pl>
2023-09-15 11:01:09 +0200

    Two numbers are positive

---
+++ Two numbers are positive/Kata.cs
@@ -0,0 +1,15 @@
+﻿namespace Two_numbers_are_positive
+{
+    public static class Kata
+    {
+        public static bool TwoArePositive(int a, int b, int c)
+        {
+            int test = 0;
+            if (a > 0) { test++; }
+            if (b > 0) { test++; }
+            if (c > 0) { test++; }
+
+            return test == 2;
+        }
+    }
+}
---
+++ Two numbers are positive/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Two_numbers_are_positive.Kata.TwoArePositive(-4, 6, 8));
