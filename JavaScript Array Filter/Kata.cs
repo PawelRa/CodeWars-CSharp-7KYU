91cbc6f11563d7e5b1f9cc0d3b6c28c710709d81
PawelRa <pawelrat@poczta.onet.pl>
2023-09-17 08:53:52 +0200

    JavaScript Array Filter

---
+++ JavaScript Array Filter/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace JavaScript_Array_Filter
+{
+    public class Kata
+    {
+        public static int[] GetEvenNumbers(int[] numbers)
+        {
+            return numbers.Where(x => x % 2 == 0).ToArray();
+        }
+    }
+}
---
+++ JavaScript Array Filter/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(String.Join(", ", JavaScript_Array_Filter.Kata.GetEvenNumbers(new int[] { 2, 4, 5, 6 })));
