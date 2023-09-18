1ebb677735b55936620e87418c1ee9c74c6347ca
PawelRa <pawelrat@poczta.onet.pl>
2023-09-18 14:21:38 +0200

    Is valid identifier? - step 1

---
+++ Is valid identifier/IdentifierChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Is_valid_identifier
+{
+    public class IdentifierChecker
+    {
+        public static bool IsValid(String idn)
+        {
+            char first = idn[0];
+            if (first != '_' && first != '$'&& Char.IsDigit(first)) { return false; }
+
+            for (int i = 1; i < idn.Length; i++)
+            {
+                if (!(Char.IsLetterOrDigit(idn[i]) || idn[i] == '_' || idn[i] != '$') ){ return false; }
+
+            }
+            return true;
+        }
+    }
+}
---
+++ Is valid identifier/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(Is_valid_identifier.IdentifierChecker.IsValid("i1"));
+
+Console.WriteLine(Is_valid_identifier.IdentifierChecker.IsValid("1i"));
+
+Console.WriteLine(Is_valid_identifier.IdentifierChecker.IsValid("okay_ok1"));
