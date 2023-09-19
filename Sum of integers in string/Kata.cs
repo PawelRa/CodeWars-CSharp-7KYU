e411500811e5a77b1c1426bbbfaf1293abef147d
PawelRa <pawelrat@poczta.onet.pl>
2023-09-19 10:34:23 +0200

    Sum of integers in string

---
+++ Sum of integers in string/Kata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_integers_in_string
+{
+    public class Kata
+    {
+        public static int SumOfIntegersInString(string s)
+        {
+            string tempStringValue = "";
+            int result = 0;
+
+            foreach (char c in s)
+            {
+                if (Char.IsNumber(c))
+                {
+                    tempStringValue += c;
+                }
+                else
+                {
+                    if (tempStringValue.Length > 0)
+                    {
+                        result += Convert.ToInt32(tempStringValue);
+                        tempStringValue = "";
+                    }
+
+                }
+            }
+            if (tempStringValue.Length > 0)
+            {
+                result += Convert.ToInt32(tempStringValue);
+                tempStringValue = "";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sum of integers in string/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Sum_of_integers_in_string.Kata.SumOfIntegersInString("12.4"));
+
+Console.WriteLine(Sum_of_integers_in_string.Kata.SumOfIntegersInString("C4t5 are 4m4z1ng."));
