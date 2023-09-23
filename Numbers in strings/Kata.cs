e1789c614cb7a02e8dc16a36a91719bef85f323a
PawelRa <pawelrat@poczta.onet.pl>
2023-09-23 11:40:44 +0200

    Numbers in strings

---
+++ Numbers in strings/Kata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers_in_strings
+{
+    public static class Kata
+    {
+        public static int Solve(string s)
+        {
+            List<int> list = new List<int>();
+            string tempNumberString = "";
+
+            foreach (char item in s)
+            {
+                if (Char.IsNumber(item))
+                {
+                    tempNumberString += item;
+                }
+                else
+                {
+                    if (tempNumberString.Length > 0)
+                    {
+                        list.Add(Convert.ToInt32(tempNumberString));
+                        tempNumberString = "";
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            if (tempNumberString.Length > 0)
+            {
+                list.Add(Convert.ToInt32(tempNumberString));
+                tempNumberString = "";
+            }
+
+            return list.Max();
+        }
+    }
+}
---
+++ Numbers in strings/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Numbers_in_strings.Kata.Solve("gh12cdy695m1"));
+
+Console.WriteLine(Numbers_in_strings.Kata.Solve("lu1j8qbbb85"));
