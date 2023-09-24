44c57817fa0113af843af2093a583ef7005edc89
PawelRa <pawelrat@poczta.onet.pl>
2023-09-24 21:54:28 +0200

    Sum a list but ignore any duplicates

---
+++ Sum a list but ignore any duplicates/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Sum_a_list_but_ignore_any_duplicates
+{
+    public class Kata
+    {
+        public static int SumNoDuplicates(int[] arr)
+        {
+            var tempList = new Dictionary<int, int>();
+
+            foreach (int i in arr)
+            {
+                if (tempList.ContainsKey(i))
+                {
+                    tempList[i]++;
+                }
+                else { tempList.Add(i, 1); }
+            }
+
+            int result = 0;
+
+            foreach (KeyValuePair<int, int> para in tempList)
+            {
+                // Console.WriteLine("Klucz: {0}, Wartość: {1}", para.Key, para.Value);
+                if (para.Value == 1) { result += para.Key; }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sum a list but ignore any duplicates/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Sum_a_list_but_ignore_any_duplicates.Kata.SumNoDuplicates(new int[] { 1, 1, 2, 2, 3 }));
