cb0ae0f6c34d918fd3527d1dd5616bbd4213bb94
PawelRa <pawelrat@poczta.onet.pl>
2023-09-26 12:30:55 +0200

    String ends with?

---
+++ String ends with/Kata.cs
@@ -0,0 +1,12 @@
+﻿namespace String_ends_with
+{
+    public class Kata
+    {
+        public static bool Solution(string str, string ending)
+        {
+            if (ending.Length > str.Length) { return false; }
+
+            return str.Substring(str.Length - ending.Length) == ending;
+        }
+    }
+}
---
+++ String ends with/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(String_ends_with.Kata.Solution("samurai", "ai"));
