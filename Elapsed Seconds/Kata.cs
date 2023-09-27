6aafffbed90c39fabe1d9a326e99a976167c5870
PawelRa <pawelrat@poczta.onet.pl>
2023-09-27 11:55:15 +0200

    Elapsed Seconds

---
+++ Elapsed Seconds/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Elapsed_Seconds
+{
+    public class Kata
+    {
+        public static int ElapsedSeconds(DateTime startDate, DateTime endDate)
+        {
+            var result = endDate - startDate;
+            return result.Hours * 360 + result.Minutes * 60 + result.Seconds;
+        }
+    }
+}
---
+++ Elapsed Seconds/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Elapsed_Seconds.Kata.ElapsedSeconds(new DateTime(2013, 1, 1, 0, 0, 1), new DateTime(2013, 1, 1, 0, 0, 2)));
+
+Console.WriteLine(Elapsed_Seconds.Kata.ElapsedSeconds(new DateTime(2013, 1, 1, 0, 0, 1), new DateTime(2010, 12, 31, 23, 56, 41)));
