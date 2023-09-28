e2653df1afc8254cba0584ac26006a5d1608f26d
PawelRa <pawelrat@poczta.onet.pl>
2023-09-28 11:26:50 +0200

    Odd or Even?

---
+++ Odd or Even/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Odd_or_Even
+{
+    public class Kata
+    {
+        public static string OddOrEven(int[] array)
+        {
+            return array.Sum() % 2 == 0 ? "even" : "odd";
+        }
+    }
+}
---
+++ Odd or Even/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Odd_or_Even.Kata.OddOrEven(new int[] { 0, 1, 5 }));
+
+Console.WriteLine(Odd_or_Even.Kata.OddOrEven(new int[] { -1023, -1, 3 }));
