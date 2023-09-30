9e361205fcabff9fec56d4395b8e212aa77bcee7
PawelRa <pawelrat@poczta.onet.pl>
2023-09-30 11:37:07 +0200

    Determine if the poker hand is flush

---
+++ Determine if the poker hand is flush/Kata.cs
@@ -0,0 +1,17 @@
+﻿namespace Determine_if_the_poker_hand_is_flush
+{
+    public class Kata
+    {
+        public static bool CheckIfFlush(string[] cards)
+        {
+            char testMark = cards[0][cards[0].Length - 1];
+
+            for (int i = 1; i < cards.Length; i++)
+            {
+                if (cards[i][cards[i].Length - 1] != testMark) { return false; }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Determine if the poker hand is flush/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(Determine_if_the_poker_hand_is_flush.Kata.CheckIfFlush(new string[] { "AS", "3S", "9S", "KS", "4S" }));
+
+Console.WriteLine(Determine_if_the_poker_hand_is_flush.Kata.CheckIfFlush(new string[] { "10D", "QD", "7D", "KD", "5D" }));
+
+Console.WriteLine(Determine_if_the_poker_hand_is_flush.Kata.CheckIfFlush(new string[] { "10D", "4S", "7H", "KC", "5S" }));
