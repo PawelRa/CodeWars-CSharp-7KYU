f4abe3d58c67a7981315f24410088a2cdd1bbd98
PawelRa <pawelrat@poczta.onet.pl>
2023-10-01 12:03:08 +0200

    Check three and two

---
+++ Check three and two/Kata.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Check_three_and_two
+{
+    public class Kata
+    {
+        public static bool CheckThreeAndTwo(string[] array)
+        {
+            var tempList = new Dictionary<string, int>();
+
+            foreach (var item in array)
+            {
+                if (tempList.ContainsKey(item))
+                {
+                    tempList[item]++;
+                }
+                else { tempList.Add(item, 1); }
+            }
+
+            foreach (KeyValuePair<string, int> para in tempList)
+            {
+                if (para.Value != 2 && para.Value != 3)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Check three and two/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Check_three_and_two.Kata.CheckThreeAndTwo(new string[] { "a", "a", "a", "b", "b" }));
+
+Console.WriteLine(Check_three_and_two.Kata.CheckThreeAndTwo(new string[] { "a", "c", "a", "c", "b" }));
