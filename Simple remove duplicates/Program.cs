1e1887ea42a442ad910aa0f818ed1a70813922b3
PawelRa <pawelrat@poczta.onet.pl>
2023-10-22 09:58:41 +0200

    Simple remove duplicates without following the order

---
+++ Simple remove duplicates/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine(String.Join(", ", Simple_remove_duplicates.Solution.solve(new int[] { 3, 4, 4, 3, 6, 3 })));
+
+Console.WriteLine(String.Join(", ", Simple_remove_duplicates.Solution.solve(new int[] { 1, 2, 1, 2, 1, 2, 3 })));
+
+Console.WriteLine(String.Join(", ", Simple_remove_duplicates.Solution.solve(new int[] { 1, 2, 3, 4 })));
+
+Console.WriteLine(String.Join(", ", Simple_remove_duplicates.Solution.solve(new int[] { 1, 1, 4, 5, 1, 2, 1 })));
---
+++ Simple remove duplicates/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_remove_duplicates
+{
+    public class Solution
+    {
+        public static int[] solve(int[] arr)
+        {
+            var testValue = new Dictionary<int, int>();
+
+            foreach (var item in arr)
+            {
+                if (testValue.ContainsKey(item))
+                {
+                    continue;
+                }
+                else
+                {
+                    testValue[item] = 1;
+                }
+            }
+
+            return new List<int>(testValue.Keys).ToArray();
+        }
+    }
+}
