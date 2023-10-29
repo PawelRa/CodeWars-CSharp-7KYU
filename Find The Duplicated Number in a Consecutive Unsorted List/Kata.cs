65b52ed3d93dbda2988847b81f4644a6f9515094
PawelRa <pawelrat@poczta.onet.pl>
2023-10-29 15:39:28 +0100

    Find The Duplicated Number in a Consecutive Unsorted List

---
+++ Find The Duplicated Number in a Consecutive Unsorted List/Kata.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Find_The_Duplicated_Number_in_a_Consecutive_Unsorted_List
+{
+    public class Kata
+    {
+        public static int FindDup(int[] arr)
+        {
+            Dictionary<int, int> arrList = new Dictionary<int, int>();
+
+            foreach (int i in arr)
+            {
+                if (arrList.ContainsKey(i))
+                {
+                    arrList[i]++;
+                }
+                else
+                {
+                    arrList.Add(i, 1);
+                }
+            }
+
+            return arrList.First(x => x.Value > 1).Key;
+        }
+    }
+}
---
+++ Find The Duplicated Number in a Consecutive Unsorted List/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Find_The_Duplicated_Number_in_a_Consecutive_Unsorted_List.Kata.FindDup(new int[] { 5, 4, 3, 2, 1, 1 }));
