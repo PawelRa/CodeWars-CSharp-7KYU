32c9cc009a8a3e0298571ae3462b777f408cbb89
PawelRa <pawelrat@poczta.onet.pl>
2023-11-01 14:30:23 +0100

    Leap Years

---
+++ Leap Years/Kata.cs
@@ -0,0 +1,14 @@
+﻿namespace Leap_Years
+{
+    public static class Kata
+    {
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0) return true;
+            if (year % 100 == 0) return false;
+            if (year % 4 == 0) return true;
+
+            return false;
+        }
+    }
+}
---
+++ Leap Years/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(Leap_Years.Kata.IsLeapYear(2020));
+
+Console.WriteLine(Leap_Years.Kata.IsLeapYear(2100));
+
+Console.WriteLine(Leap_Years.Kata.IsLeapYear(2000));
