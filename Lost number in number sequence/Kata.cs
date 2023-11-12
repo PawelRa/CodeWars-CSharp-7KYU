8343209373a4835e5a0f527972d391f5d4ca1ec3
PawelRa <pawelrat@poczta.onet.pl>
2023-11-12 18:28:18 +0100

    Lost number in number sequence

---
+++ Lost number in number sequence/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lost_number_in_number_sequence
+{
+    public class Kata
+    {
+        public static int FindDeletedNumber(List<int> startingList, List<int> mixedList)
+        {
+            startingList.Sort();
+            mixedList.Sort();
+            if (startingList.Count == mixedList.Count) { return 0; }
+            if (startingList.Count == 1 && (mixedList.Count == 0 || mixedList == null)) return startingList[0];
+
+            for (int i = 0; i < mixedList.Count; i++)
+            {
+                if (startingList[i] != mixedList[i]) { return startingList[i]; }
+            }
+            return startingList[startingList.Count - 1];
+        }
+    }
+}
---
+++ Lost number in number sequence/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine(Lost_number_in_number_sequence.Kata.FindDeletedNumber(new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new List<int>() { 5, 7, 1, 9, 4, 8, 2, 3 }));
+
+Console.WriteLine(Lost_number_in_number_sequence.Kata.FindDeletedNumber(new List<int>() { 1 }, new List<int>()));
