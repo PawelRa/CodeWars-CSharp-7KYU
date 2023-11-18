a06ab1729f08d23a9c9673c2dff0b4d4e1a479b1
PawelRa <pawelrat@poczta.onet.pl>
2023-11-18 22:44:29 +0100

    Get the mean of an array

---
+++ Get the mean of an array/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Get_the_mean_of_an_array
+{
+    public class Kata
+    {
+        public static int GetAverage(int[] marks)
+        {
+            return (int)Math.Floor(marks.Average());
+        }
+    }
+}
---
+++ Get the mean of an array/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine(Get_the_mean_of_an_array.Kata.GetAverage(new int[] { 5, 10 }));
